d07b6c97652be29839d1f027d2e31446995e1a1b
Evgeniyyd <brai.kel@icloud.com>
2025-03-20 15:48:58 +0300

    add Solution task0107

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0107/Test.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task01.task0107;
+
+import java.util.Comparator;
+
+public class Test {
+    private Comparator<String> comparator;
+
+    private Comparator<String> comparator() {
+        return comparator;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+}
