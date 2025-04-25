f06cb2968c06e1856b6dce2b82fcc8e6c4fe368c
Evgeniyyd <brai.kel@icloud.com>
2025-04-25 18:28:01 +0300

    добавил в классы переопределеные методы интерфейса  Flyable и изменил реализации в классах

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1717/Eagle.java
@@ -0,0 +1,10 @@
+package com.javarush.task.pro.task17.task1717;
+
+public class Eagle implements Flyable {
+    int speed = 180;
+    @Override
+    public int getMaxSpeed() {
+        int speed = 180;
+        return speed;
+    }
+}
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1717/Raven.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task17.task1717;
+
+public class Raven implements Flyable {
+
+
+    @Override
+    public int getMaxSpeed() {
+        int speed = 48;
+        return speed;
+    }
+}
