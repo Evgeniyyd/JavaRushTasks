d095cec5ade9a62f34922290a78a29dd865056de
Evgeniyyd <brai.kel@icloud.com>
2025-05-16 20:23:14 +0300

    реализовал проверку на null при помощи класса Optional и методов ofNullable(), orElse() далее сделал вывод через метод forEach

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task18/task1819/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task18.task1819;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/* 
+Как быть, если в списке есть элемент null
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        List<String> strings = new ArrayList<>();
+        Collections.addAll(strings, "first", "second", null, "fourth", "fifth");
+
+        printList(strings);
+    }
+
+    public static void printList(List<String> list) {
+        String text = "Этот элемент равен null";
+       Stream<String> objectStream =list.stream().map(s -> Optional.ofNullable(s).orElse(text));
+                objectStream.forEach(System.out :: println);
+
+
+
+
+    }
+}
