bc343c7b6648776b9d455a551b8cdd5f7765225d
Evgeniyyd <brai.kel@icloud.com>
2025-06-04 16:22:42 +0300

    создал новый клас и добавил константу и метод

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1421/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task14.task1421;
+
+/* 
+Singleton
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Singleton instance = Singleton.getInstance();
+    }
+}
+//константа это - всегда переменная и оно некогда неизменяет значение
+// прочитать про  Singleton
