3e4af911ec865c003f40433b3e6640a3676d59d5
Evgeniyyd <brai.kel@icloud.com>
2025-06-08 13:00:31 +0300

    реализовали -с

---
+++ 2.JavaCore/src/com/javarush/task/task17/task1710/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task17.task1710;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+/* 
+CRUD
+*/
+
+public class Solution {
+    public static List<Person> allPeople = new ArrayList<Person>();
+
+    static {
+        allPeople.add(Person.createMale("Иванов Иван", new Date()));  //сегодня родился    id=0
+        allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
+    }
+
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat input = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+         if (args[0].equals("-c")) {
+            Person person = null;
+            if (args[2].equalsIgnoreCase("м")) {
+                person = Person.createMale(args[1],input.parse(args[3]));
+            } else if (args[2].equalsIgnoreCase("ж")) {
+                person = Person.createFemale(args[1], input.parse(args[3]));
+            }
+            allPeople.add(person);
+            System.out.println(allPeople.indexOf(person));
+        }
+
+
+    }
+}
