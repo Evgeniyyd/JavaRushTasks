d9ae69116f6888e7c17fbdbde91df8f1cb267402
Evgeniyyd <brai.kel@icloud.com>
2025-07-21 23:05:03 +0300

    решена

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1802/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task18.task1802;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+/* 
+Минимальный байт
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try (FileInputStream fileInputStream = new FileInputStream(bufferedReader.readLine())) {
+            int min = Integer.MAX_VALUE;
+            while (fileInputStream.available() > 0) {
+                int read = fileInputStream.read();
+                if (min > read) {
+                    min = read;
+                }
+
+            }
+            System.out.println(min);
+
+        }
+    }
+}
