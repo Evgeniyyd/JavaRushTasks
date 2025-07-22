e4d146e378f86958e8f03395106b746d90b84825
Evgeniyyd <brai.kel@icloud.com>
2025-07-22 10:17:45 +0300

    Поиск Максимального байта

--- 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1801/Solution.java
@@ -11,19 +11,15 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-       try( FileInputStream fileInputStream = new FileInputStream(bufferedReader.readLine())){
-
-           int max =0;
-           while (fileInputStream.available() > 0){
-               int read = fileInputStream.read();
-               if (read > max){
-                   max = read;
-               }
-           }
-           System.out.println(max);
-
-
-
-       }
+        try (FileInputStream fileInputStream = new FileInputStream(bufferedReader.readLine())) {
+            int max = Integer.MIN_VALUE;
+            while (fileInputStream.available() > 0) {
+                int read = fileInputStream.read();
+                if (read > max) {
+                    max = read;
+                }
+            }
+            System.out.println(max);
+        }
     }
 }
