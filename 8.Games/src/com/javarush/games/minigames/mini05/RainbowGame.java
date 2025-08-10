a0f6289b96af0c47d4f83893e57b9b8032dd2889
Evgeniyyd <brai.kel@icloud.com>
2025-08-10 11:21:25 +0300

    решена

---
+++ 8.Games/src/com/javarush/games/minigames/mini05/RainbowGame.java
@@ -0,0 +1,92 @@
+package com.javarush.games.minigames.mini05;
+
+import com.javarush.engine.cell.Game;
+import com.javarush.engine.cell.Color;
+
+/* 
+Цвета радуги
+*/
+
+public class RainbowGame extends Game {
+    @Override
+    public void initialize(){
+        setScreenSize(10,7);
+        setCellColor(0,0, Color.RED);
+        setCellColor(1,0, Color.RED);
+        setCellColor(2,0, Color.RED);
+        setCellColor(3,0, Color.RED);
+        setCellColor(4,0, Color.RED);
+        setCellColor(5,0, Color.RED);
+        setCellColor(6,0, Color.RED);
+        setCellColor(7,0, Color.RED);
+        setCellColor(8,0, Color.RED);
+        setCellColor(9,0, Color.RED);
+
+        setCellColor(0,1, Color.ORANGE);
+        setCellColor(1,1, Color.ORANGE);
+        setCellColor(2,1, Color.ORANGE);
+        setCellColor(3,1, Color.ORANGE);
+        setCellColor(4,1, Color.ORANGE);
+        setCellColor(5,1, Color.ORANGE);
+        setCellColor(6,1, Color.ORANGE);
+        setCellColor(7,1, Color.ORANGE);
+        setCellColor(8,1, Color.ORANGE);
+        setCellColor(9,1, Color.ORANGE);
+
+        setCellColor(0,2, Color.YELLOW);
+        setCellColor(1,2, Color.YELLOW);
+        setCellColor(2,2, Color.YELLOW);
+        setCellColor(3,2, Color.YELLOW);
+        setCellColor(4,2, Color.YELLOW);
+        setCellColor(5,2, Color.YELLOW);
+        setCellColor(6,2, Color.YELLOW);
+        setCellColor(7,2, Color.YELLOW);
+        setCellColor(8,2, Color.YELLOW);
+        setCellColor(9,2, Color.YELLOW);
+
+        setCellColor(0,3, Color.GREEN);
+        setCellColor(1,3, Color.GREEN);
+        setCellColor(2,3, Color.GREEN);
+        setCellColor(3,3, Color.GREEN);
+        setCellColor(4,3, Color.GREEN);
+        setCellColor(5,3, Color.GREEN);
+        setCellColor(6,3, Color.GREEN);
+        setCellColor(7,3, Color.GREEN);
+        setCellColor(8,3, Color.GREEN);
+        setCellColor(9,3, Color.GREEN);
+
+        setCellColor(0,4, Color.BLUE);
+        setCellColor(1,4, Color.BLUE);
+        setCellColor(2,4, Color.BLUE);
+        setCellColor(3,4, Color.BLUE);
+        setCellColor(4,4, Color.BLUE);
+        setCellColor(5,4, Color.BLUE);
+        setCellColor(6,4, Color.BLUE);
+        setCellColor(7,4, Color.BLUE);
+        setCellColor(8,4, Color.BLUE);
+        setCellColor(9,4, Color.BLUE);
+
+        setCellColor(0,5, Color.INDIGO);
+        setCellColor(1,5, Color.INDIGO);
+        setCellColor(2,5, Color.INDIGO);
+        setCellColor(3,5, Color.INDIGO);
+        setCellColor(4,5, Color.INDIGO);
+        setCellColor(5,5, Color.INDIGO);
+        setCellColor(6,5, Color.INDIGO);
+        setCellColor(7,5, Color.INDIGO);
+        setCellColor(8,5, Color.INDIGO);
+        setCellColor(9,5, Color.INDIGO);
+
+        setCellColor(0,6, Color.VIOLET);
+        setCellColor(1,6, Color.VIOLET);
+        setCellColor(2,6, Color.VIOLET);
+        setCellColor(3,6, Color.VIOLET);
+        setCellColor(4,6, Color.VIOLET);
+        setCellColor(5,6, Color.VIOLET);
+        setCellColor(6,6, Color.VIOLET);
+        setCellColor(7,6, Color.VIOLET);
+        setCellColor(8,6, Color.VIOLET);
+        setCellColor(9,6, Color.VIOLET);
+    }
+
+}
