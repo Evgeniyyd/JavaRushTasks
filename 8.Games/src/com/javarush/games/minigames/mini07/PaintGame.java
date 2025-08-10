f777c9cdbbcba48a50b53d3a31f744d5bd5d7c39
Evgeniyyd <brai.kel@icloud.com>
2025-08-10 11:22:18 +0300

    решена

---
+++ 8.Games/src/com/javarush/games/minigames/mini07/PaintGame.java
@@ -0,0 +1,34 @@
+package com.javarush.games.minigames.mini07;
+
+import com.javarush.engine.cell.Color;
+import com.javarush.engine.cell.Game;
+
+/* 
+Работа с мышью
+*/
+
+public class PaintGame extends Game {
+
+    @Override
+    public void initialize() {
+        setScreenSize(5, 5);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                setCellColor(i, j, Color.WHITE);
+            }
+
+        }
+    }
+
+    @Override
+    public void onMouseLeftClick(int с, int d) {
+        setCellColor(с, d, Color.GREEN);
+    }
+
+
+    @Override
+    public void onMouseRightClick(int a, int b) {
+        setCellColor(a, b, Color.ORANGE);
+    }
+}
+
