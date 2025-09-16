2cff1dba6abc4874977c8fb732df4258d20e1465
Evgeniyyd <brai.kel@icloud.com>
2025-09-16 11:06:12 +0300

    1631

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1631/common/BmpReader.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task16.task1631.common;
+
+public class BmpReader implements ImageReader{
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task16/task1631/common/ImageTypes.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task16.task1631.common;
+
+public enum ImageTypes {
+    BMP,
+    JPG,
+    PNG,
+    RAW
+
+}
