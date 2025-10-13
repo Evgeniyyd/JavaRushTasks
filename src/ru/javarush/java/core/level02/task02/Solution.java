7e71f50d8a26937a6a82660b622e02ff8d29fd43
Evgeniyyd <brai.kel@icloud.com>
2025-10-13 19:48:50 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task02/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level02.task02;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем и инициализируем переменные в одной строке
+int firstRoll =7; int secondRoll = 3; int totalScore = firstRoll + secondRoll;
+        System.out.println(totalScore);
+
+        // Выводим значение переменной totalScore на экран
+
+    }
+}
