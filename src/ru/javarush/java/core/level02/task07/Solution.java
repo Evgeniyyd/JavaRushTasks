1860d3fdc027f9b8b448a9212e2ae37de946bf3a
Evgeniyyd <brai.kel@icloud.com>
2025-10-13 20:09:51 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task07/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level02.task07;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Внутренние двойные кавычки экранируем обратным слэшем
+String heroQuote = "\"Поехали!\"";
+        System.out.println(heroQuote);
+
+        // Выводим значение переменной на экран
+
+    }
+}
