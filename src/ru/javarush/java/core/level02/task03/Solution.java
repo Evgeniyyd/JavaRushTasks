bb36c42035d418372c8888d2faa5a953a718fdca
Evgeniyyd <brai.kel@icloud.com>
2025-10-13 19:53:23 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task03/Solution.java
@@ -0,0 +1,22 @@
+package ru.javarush.java.core.level02.task03;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем общее количество призов и число команд
+int prizes =15;
+int teams =4;
+        System.out.println(prizes / teams);
+        System.out.println( prizes % teams);
+
+        // Вычисляем, сколько целых призов получит каждая команда (целочисленное деление)
+
+
+        // Вычисляем, сколько призов останется после раздачи (оператор %)
+
+
+        // Выводим количество призов на команду
+
+        // Выводим остаток призов
+
+    }
+}
