b20897c7024afb0e9b6ce89ec61eb5b2f5a91885
Evgeniyyd <brai.kel@icloud.com>
2025-10-13 19:10:54 +0300

    решена

---
+++ src/ru/javarush/java/core/level01/task09/Solution.java
@@ -0,0 +1,23 @@
+package ru.javarush.java.core.level01.task09;
+
+public class Solution
+{
+    public static void main(String[] args) {
+        // Объявляем переменные типа int и присваиваем им значения
+int itemPrice = 4;
+int itemCount =3;
+int discount  =2;
+int totalCost  = (itemPrice + itemCount) * discount;
+String finalMessage =  "Итоговая стоимость: " + totalCost;
+        System.out.println(finalMessage);
+
+        // Вычисляем итоговую стоимость заказа
+
+
+        // Формируем итоговое сообщение
+
+
+        // Выводим итоговое сообщение на экран
+
+    }
+}
