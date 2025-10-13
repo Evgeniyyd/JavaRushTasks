118eff4644ce62c0b895217bb379a3df6eeec170
Evgeniyyd <brai.kel@icloud.com>
2025-10-13 20:04:28 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task06/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level02.task06;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменные и присваиваем им значения
+String firstName ="Luke";
+String  lastName ="Skywalker";
+        System.out.println(firstName+" "+ lastName);
+
+        // Объединяем имя и фамилию через пробел
+
+
+        // Выводим получившееся полное имя
+
+    }
+}
