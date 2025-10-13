5c50e7a13a6d14f6ef3d2abf83c8f7d473498385
Evgeniyyd <brai.kel@icloud.com>
2025-10-13 19:59:35 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task04/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level02.task04;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем и инициализируем уровень жизни героя
+        int heroHealth = 10;
+        heroHealth++;
+        System.out.println(heroHealth);
+        heroHealth--;
+        System.out.println(heroHealth);
+        // Увеличиваем значение на 1 (инкремент) и выводим результат
+
+
+        // Уменьшаем значение на 1 (декремент) и снова выводим результат
+
+    }
+}
