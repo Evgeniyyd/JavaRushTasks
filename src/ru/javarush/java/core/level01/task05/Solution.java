410735167838a496b2e7cd6c71528126c75a12aa
Evgeniyyd <brai.kel@icloud.com>
2025-10-13 17:41:46 +0300

    решена

---
+++ src/ru/javarush/java/core/level01/task05/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level01.task05;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную типа String с именем myMessage
+
+String myMessage ="I ❤️ YOU";
+        System.out.println(myMessage);
+        // Выводим значение переменной myMessage на экран
+
+    }
+}
---
+++ src/ru/javarush/java/core/level01/task06/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level01.task06;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную currentYear и присваиваем ей значение 2025
+        int currentYear = 2025;
+        int projectAge = currentYear - 2019;
+        System.out.println(projectAge);
+    }
+
+        // Объявляем переменную projectAge
+
+
+        // Вычисляем возраст проекта как разницу между currentYear и 2019
+
+
+        // Выводим возраст проекта на экран
+
+    }
