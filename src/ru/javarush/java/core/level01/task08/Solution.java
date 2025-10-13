1f127cf4477028ab1af309a5d49e075a7a06ee17
Evgeniyyd <brai.kel@icloud.com>
2025-10-13 18:11:44 +0300

    решена

---
+++ src/ru/javarush/java/core/level01/task08/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level01.task08;
+
+public class Solution
+{
+    public static void main(String[] args) {
+        // Объявляем переменную типа String для имени пользователя
+String userName = "Alice";
+
+        // Объявляем переменную типа int для возраста пользователя
+int userAge = 20;
+ String  profileInfo ="Имя: " +userName+", Возраст: " + userAge;
+        System.out.println(profileInfo);
+        // Создаем строку, объединяющую имя и возраст пользователя
+
+
+        // Выводим информацию о профиле на экран
+
+    }
+}
