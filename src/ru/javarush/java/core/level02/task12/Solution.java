b2e7bdee84faec954246e7731929567fc16a66b3
Evgeniyyd <brai.kel@icloud.com>
2025-10-14 21:54:41 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task12/Solution.java
@@ -0,0 +1,21 @@
+package ru.javarush.java.core.level02.task12;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Очки приходят как текстовые строки с отрицательными числами
+       String string ="-10";
+       String str = "-25";
+        int parsed = Integer.parseInt(string);
+        int parsed1 = Integer.parseInt(str);
+        System.out.println( parsed + parsed1);
+
+        // Преобразуем строки в числа типа int
+
+
+        // Складываем результаты двух раундов
+
+
+        // Выводим общий счёт на экран
+
+    }
+}
