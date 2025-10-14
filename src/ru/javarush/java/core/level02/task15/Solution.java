4a1bf2f53b7cd6fa6a9c6cf001a5438dddf73c2a
Evgeniyyd <brai.kel@icloud.com>
2025-10-14 22:50:02 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task15/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level02.task15;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменные разных типов и присваиваем им произвольные значения
+        byte b = 1;
+        int i = 4;
+        double d = 8.0;
+        System.out.println("byte - " + b + " байт");
+        System.out.println("int - " + i + " байт");
+        System.out.println("double - " + (int)d + " байт");
+        // Выводим информацию о размере каждого типа данных
+
+
+    }
+}
