8ad8e3cc695fa395ed24eadfb708f44acd7999ac
Evgeniyyd <brai.kel@icloud.com>
2025-10-14 22:34:38 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task13/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level02.task13;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменные с информацией о персон
+        byte level = 34;
+        int goldAmount =35;
+        double rating=34.5;
+        String characterName="35";
+        System.out.println(level);
+        System.out.println(goldAmount);
+        System.out.println(rating);
+        System.out.println(characterName);
+        // Выводим все данные на экран
+
+
+    }
+}
