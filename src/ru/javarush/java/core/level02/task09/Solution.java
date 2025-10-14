a63f8a0933fcaf9436da9eeee4abbcc48a81a135
Evgeniyyd <brai.kel@icloud.com>
2025-10-14 21:40:19 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task09/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level02.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную типа int и присваиваем ей произвольное целое значение (код доступа)
+        int code = 6;
+        String string = String.valueOf(code);
+        System.out.println(string);
+
+        // Преобразуем целочисленное значение accessCode в строку
+
+
+        // Выводим полученную строку на экран
+
+    }
+}
