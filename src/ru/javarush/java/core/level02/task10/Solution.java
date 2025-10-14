4780c4a7b81d5cfcca5e65f53b724bed4d301817
Evgeniyyd <brai.kel@icloud.com>
2025-10-14 21:46:08 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task10/Solution.java
@@ -0,0 +1,19 @@
+package ru.javarush.java.core.level02.task10;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную типа int для хранения номера рейса
+     int number =786;
+     String sity = "Минск";
+        System.out.println("Рейс"+" "+ number+ " "+"до "+ sity);
+
+        // Объявляем переменную типа String для хранения города назначения
+
+
+        // Объединяем номер рейса и город назначения в одну строку с помощью оператора +
+
+
+        // Выводим итоговую строку на экран
+
+    }
+}
