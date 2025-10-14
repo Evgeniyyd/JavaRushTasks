cd341c903b6f5711e26c9a5839ab931c4ca5bc3b
Evgeniyyd <brai.kel@icloud.com>
2025-10-14 22:38:25 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task14/Solution.java
@@ -0,0 +1,22 @@
+package ru.javarush.java.core.level02.task14;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную projectName и задаем ей исходное название проекта
+ String projectName ="Java";
+ String  newProjectName = projectName;
+  newProjectName = "Java Programming";
+        System.out.println(projectName);
+        System.out.println(newProjectName);
+
+        // Объявляем переменную newProjectName и инициализируем ее значением из projectName
+
+
+        // Изменяем значение копии названия проекта
+
+
+        // Выводим на экран оригинальное и измененное название проекта
+
+
+    }
+}
