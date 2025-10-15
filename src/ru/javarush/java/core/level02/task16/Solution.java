74b0e026e3af5ce5ec2e0fe0687ae1e3d54418ad
Evgeniyyd <brai.kel@icloud.com>
2025-10-15 16:48:27 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task16/Solution.java
@@ -0,0 +1,24 @@
+package ru.javarush.java.core.level02.task16;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем две переменные типа int и инициализируем их начальными значениями
+        int account1 = 100;
+        int account2 = 200;
+        account2 = account2 + account1;
+        account1 = 0;
+        account1 +=50;
+        System.out.println(account1);
+        System.out.println(account2);
+
+        // Присваиваем переменной account2 значение account2 + account1 (переводим деньги)
+
+
+        // На счет1 приходит бонус в размере 50 и мы увеличиваем account1 на 50
+
+
+        // Выводим на экран конечные значения обеих переменных
+
+
+    }
+}
