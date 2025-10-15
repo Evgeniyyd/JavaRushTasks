cfc756896d60c0a8218e369429a3d66c50fa985e
Evgeniyyd <brai.kel@icloud.com>
2025-10-15 20:14:04 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task20/Solution.java
@@ -0,0 +1,22 @@
+package ru.javarush.java.core.level02.task20;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект Scanner для чтения данных с консоли
+      Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int age = scanner.nextInt();
+        System.out.println("Меня зовут " + name +", мне "+ age+ " лет.");
+
+        // Считываем имя пользователя (строка)
+
+
+        // Считываем возраст пользователя (целое число)
+
+
+        // Выводим отформатированную визитную карточку
+
+    }
+}
