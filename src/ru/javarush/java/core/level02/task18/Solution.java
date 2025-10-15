90ece54e6e0fdd2726b52f769865f4fd9cb02975
Evgeniyyd <brai.kel@icloud.com>
2025-10-15 20:01:48 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task18/Solution.java
@@ -0,0 +1,26 @@
+package ru.javarush.java.core.level02.task18;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем объект Scanner для чтения данных с клавиатуры
+   Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int age = scanner.nextInt();
+        System.out.println(age);
+        System.out.println(name);
+
+
+
+        // Запрашиваем и считываем имя игрока
+
+
+        // Запрашиваем и считываем возраст игрока
+
+
+        // Сначала выводим возраст, затем имя, каждый с новой строки
+
+
+    }
+}
