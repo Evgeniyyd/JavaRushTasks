0c0d12496fa083485e28ab25f7df7b6d6ffef876
Evgeniyyd <brai.kel@icloud.com>
2025-10-15 16:52:09 +0300

    решена

---
+++ src/ru/javarush/java/core/level02/task17/Solution.java
@@ -0,0 +1,18 @@
+package ru.javarush.java.core.level02.task17;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Создаем Scanner для чтения данных с клавиатуры
+Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        System.out.println(string);
+
+        // Считываем одну строку как пароль
+
+
+        // Выводим введенный пароль на экран
+
+    }
+}
