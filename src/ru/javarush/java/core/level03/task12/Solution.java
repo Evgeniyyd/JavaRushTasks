004124943ff0affea8282b73c331b2be9cf5cf09
Evgeniyyd <brai.kel@icloud.com>
2025-10-27 18:21:10 +0300

    решена

---
+++ src/ru/javarush/java/core/level03/task12/Solution.java
@@ -0,0 +1,12 @@
+package ru.javarush.java.core.level03.task12;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную roomTemperature типа int
+int roomTemperature =0;
+
+boolean isComfortable = false;
+if (roomTemperature >= 20 && roomTemperature <= 25 ){
+    System.out.println(isComfortable);
+
+}}}
