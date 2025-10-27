2e8dd7b7ea7b827d0e46ee7ca58b64198c878c66
Evgeniyyd <brai.kel@icloud.com>
2025-10-27 16:38:41 +0300

    решена

---
+++ src/ru/javarush/java/core/level03/task10/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level03.task10;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем и присваиваем счёт нашей команды и соперника
+int  ourScore = 54;
+int  opponentScore =76;
+boolean weWon = (ourScore > opponentScore);
+        System.out.println(weWon);
+
+        // weWon — результат логического сравнения: true, если наш счёт больше
+
+
+        // Выводим значение переменной weWon на экран
+
+    }
+}
