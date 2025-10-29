d62cb46be9ff31b439b548a826aab1cd990a7e20
Evgeniyyd <brai.kel@icloud.com>
2025-10-29 16:23:11 +0300

    решена

---
+++ src/ru/javarush/java/core/level03/task19/Solution.java
@@ -0,0 +1,15 @@
+package ru.javarush.java.core.level03.task19;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную currentHour, которая хранит текущий час (от 0 до 23)
+        int currentHour = 22;
+        String greetingMessage = (currentHour < 12 ? "Доброе утро" : "Добрый день");
+        System.out.println(greetingMessage);
+        // Используем тернарный оператор для выбора приветствия
+
+
+        // Выводим приветственное сообщение на экран
+
+    }
+}
