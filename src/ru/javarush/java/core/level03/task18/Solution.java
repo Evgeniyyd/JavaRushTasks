56fcea4594cb205c24df4a1be936b672dd4be6f9
Evgeniyyd <brai.kel@icloud.com>
2025-10-29 16:17:52 +0300

    решена

---
+++ src/ru/javarush/java/core/level03/task18/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level03.task18;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную orderNumber и присваиваем ей значение
+        int orderNumber = 76;
+        String orderStatus = (orderNumber % 2 == 0 ? "Чётный" : "Нечётный");
+        System.out.println(orderStatus);
+
+        // Используем тернарный оператор для определения чётности orderNumber
+
+
+        // Выводим результат на экран
+
+    }
+}
