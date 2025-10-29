0e8932bd4e18c322bc1e1c71895d1742bab826ee
Evgeniyyd <brai.kel@icloud.com>
2025-10-29 16:07:48 +0300

    решена

---
+++ src/ru/javarush/java/core/level03/task17/Solution.java
@@ -0,0 +1,17 @@
+package ru.javarush.java.core.level03.task17;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем две переменные для времени спортсменов
+int alexsTime =45;
+int mikesTime =23;
+ int fastestTime = (alexsTime < mikesTime ? alexsTime : mikesTime);
+        System.out.println(fastestTime);
+
+        // Используем тернарный оператор для определения минимального времени
+
+
+        // Выводим на экран наименьшее время
+
+    }
+}
