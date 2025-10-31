ef493d89bf03822fe33292e1f37edd3b5360ef58
Evgeniyyd <brai.kel@icloud.com>
2025-10-31 16:58:27 +0300

    решена

---
+++ src/ru/javarush/java/core/level04/task03/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level04.task03;
+
+import java.util.stream.IntStream;
+
+public class Solution {
+    public static void main(String[] args) {
+        int i =2;
+        while (i <=10) {
+            System.out.println(i);
+            i+=2;
+
+        }
+
+    }
+}
+
