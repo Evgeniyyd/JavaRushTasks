172e08d358223bc2a3e2b07ededf7f324e4b4624
Evgeniyyd <brai.kel@icloud.com>
2025-11-05 16:35:17 +0300

    решена

---
+++ src/ru/javarush/java/core/level04/task08/Solution.java
@@ -0,0 +1,13 @@
+package ru.javarush.java.core.level04.task08;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Используем цикл for для одновременного изменения двух переменных
+        for (int x = 1, y = 5; x <=5; x++, y--) {
+
+                System.out.println(x + " " + y);
+            }
+            // На каждой итерации выводим текущие значения x и y через пробел
+
+        }
+    }
