cc6df74f6a947f4ba04d27cdafbb174aafa2a889
Evgeniyyd <brai.kel@icloud.com>
2025-11-05 16:43:38 +0300

    решена

---
+++ src/ru/javarush/java/core/level04/task09/Solution.java
@@ -0,0 +1,16 @@
+package ru.javarush.java.core.level04.task09;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Объявляем переменную gameVersion и присваиваем ей значение 10
+        int gameVersion =10;
+        do {
+            System.out.println("Загрузка данных...");
+            System.out.println(gameVersion);
+        }while (false);
+
+        }
+
+
+
+    }
