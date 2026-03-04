15baf2c767274f58aecbd05c898668ba849c5452
Evgeniyyd <brai.kel@icloud.com>
2026-03-04 18:49:29 +0300

    последовательное работа потоков

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1629/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task16.task1629;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Только по-очереди!
+*/
+
+public class Solution {
+    public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws InterruptedException {
+        Read3Strings t1 = new Read3Strings();
+        Read3Strings t2 = new Read3Strings();
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        //add your code here - добавьте код тут
+
+        t1.printResult();
+        t2.printResult();
+    }
+
+    public static class Read3Strings extends Thread {
+ String line1,line2,line3;
+        @Override
+        public void run() {
+            try {
+               line1= reader.readLine();
+               line2= reader.readLine();
+               line3= reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public void printResult() {
+            System.out.println( line1+ " "+line2+" "+line3);
+
+        }
+    }
+}
