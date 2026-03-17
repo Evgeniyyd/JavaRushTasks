d4787bcbab8f68ab929f66db191e56e2fe3a3f81
Evgeniyyd <brai.kel@icloud.com>
2026-03-18 00:08:21 +0300

    Дописать реализацию задания GRUD2

--- 2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1711/Solution.java
@@ -1,10 +1,8 @@
 package com.javarush.task.task17.task1711;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
 /* 
 CRUD 2
@@ -18,15 +16,45 @@ public class Solution {
         allPeople.add(Person.createMale("Петров Петр", new Date()));  //сегодня родился    id=1
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+        Person person = null;
         if (args[0].equals("-c")) {
-            for (int i = 1; i < args.length; i+=3) {
+            for (int i = 1; i < args.length; i += 3) {
                 String name = args[i];
-               String sex = args[i+1];
-               String bd = args[i+2];
+                String sex = args[i + 1];
+                Date date = dateFormat.parse(args[i + 2]);
+                if (sex.equalsIgnoreCase("M")) {
+                    person = Person.createMale(name, date);
+                } else if (sex.equalsIgnoreCase("Ж")) {
+                    person = Person.createFemale(name, date);
+                }
+                allPeople.add(person);
+                int index = allPeople.indexOf(person);
+                System.out.println(index);
+            }
+        } else if (args[0].equals("-u")) {
+            for (int i = 1; i < args.length; i+=4) {
+                String id = args[i];
+                String name = args[i=1];
+                String sex = args[i+2];
+                Date date = dateFormat.parse(args[i + 3]);
+                int index = Integer.parseInt(id);
+                if (index >=0 && index < allPeople.size()){
+                    if (sex.equalsIgnoreCase("Ж")){
+                       person = Person.createFemale(name, date);
+
+                    } else if (sex.equalsIgnoreCase("M")) {
+                       person = Person.createMale(name, date);
+                    }
+                    allPeople.set(index,person);
+
+
+
+                }
             }
 
-
         }
+
     }
 }
