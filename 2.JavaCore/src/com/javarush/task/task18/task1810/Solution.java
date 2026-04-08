307cb678779a5f762bbafc8a0746870a9ff0aac7
Evgeniyyd <brai.kel@icloud.com>
2026-04-08 19:15:57 +0300

    изменить условия реализации

--- 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
@@ -1,27 +1,27 @@
 package com.javarush.task.task18.task1810;
 
 import java.io.*;
-import java.util.ArrayList;
+import java.nio.file.Files;
+
+
 
 /* +
 DownloadException
 */
 
 public class Solution {
-    public static void main(String[] args) throws DownloadException, IOException {
+    public static void main(String[] args) throws DownloadException, IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        FileInputStream inputStream = new FileInputStream(reader.readLine());
-        ArrayList<Integer> list = new ArrayList<>();
-        while (inputStream.available() != 0) {
-            int read = inputStream.read();
-            list.add(read);
-            for (int i = 0; i < list.size(); i++) {
-                if (list.get(i) < 1000) {
-                    inputStream.close();
+        try (FileInputStream inputStream = new FileInputStream((reader.readLine()))) {
+        while (inputStream.read() != -1) {
+                File file = new File(reader.readLine());
+                long length = Files.size(file.toPath());
+                if (length < 1000) {
+                    throw new DownloadException();
                 }
             }
         }
     }
-    public static class DownloadException   extends Exception {
+    public static class DownloadException extends Exception {
     }
 }
