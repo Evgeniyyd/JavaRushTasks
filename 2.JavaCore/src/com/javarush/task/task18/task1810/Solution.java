fc152963ff25313004f3bd400d760c96db4da6c5
Evgeniyyd <brai.kel@icloud.com>
2026-04-09 12:25:46 +0300

    подсчёт размара файла

--- 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1810/Solution.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task18.task1810;
 
 import java.io.*;
-import java.nio.file.Files;
 
 
 
@@ -10,13 +9,12 @@ DownloadException
 */
 
 public class Solution {
-    public static void main(String[] args) throws DownloadException, IOException{
+    public static void main(String[] args) throws DownloadException, IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        try (FileInputStream inputStream = new FileInputStream((reader.readLine()))) {
-        while (inputStream.read() != -1) {
-                File file = new File(reader.readLine());
-                long length = Files.size(file.toPath());
-                if (length < 1000) {
+        while (true) {
+            try (FileInputStream inputStream = new FileInputStream((reader.readLine()))) {
+                int available = inputStream.available();
+                if (available < 1000) {
                     throw new DownloadException();
                 }
             }
