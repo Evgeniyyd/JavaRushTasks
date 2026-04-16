c51f1df112a29d9122aab3609092907b84f497af
Evgeniyyd <brai.kel@icloud.com>
2026-04-16 15:07:41 +0300

    добавить реализацию

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1827/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task18.task1827;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Прайсы
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+            String fileName = console.readLine();
+           try( BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))){
+
+
+            int max = 0;
+            while (reader.ready()) {
+                String string = reader.readLine();
+                int id = Integer.parseInt(string.substring(0, 8).trim());
+                if (id > max) {
+                    max = id;
+                }
+            }
+            String productName = args[1];
+            String price = args[2];
+            String quantity = args[3];
+            writer.newLine();
+            writer.write(max + productName + price + quantity);
+        }
+    }
+}
