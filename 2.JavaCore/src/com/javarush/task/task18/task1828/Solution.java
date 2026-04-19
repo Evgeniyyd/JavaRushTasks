6bec2bbd35d83a8a3095a30d34ece7a824a2befd
Evgeniyyd <brai.kel@icloud.com>
2026-04-19 20:43:20 +0300

    изменить реализацию

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task18.task1828;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Прайсы 2
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try (BufferedReader reader = new BufferedReader(new FileReader(bufferedReader.readLine()))) {
+            ArrayList<String> list = new ArrayList<>();
+            while (reader.ready()) {
+                String string = reader.readLine();
+                list.add(string);
+            }
+                if (args[0].equals("-u")) {
+                    String idIsArgs = args[1];
+                  for (String str : list){
+                      String idIsFile = str.substring(0, 8).trim();
+                      if (idIsArgs.equals(idIsFile)){
+
+                      }
+                  }
+            }
+        }
+    }
+}
