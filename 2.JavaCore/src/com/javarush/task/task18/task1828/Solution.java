8ee19adbaaac8f6a4887d684f592285ad25d7278
Evgeniyyd <brai.kel@icloud.com>
2026-04-20 21:00:04 +0300

    изменить реализацию

--- 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
@@ -2,7 +2,6 @@ package com.javarush.task.task18.task1828;
 
 import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
 
 /* 
 Прайсы 2
@@ -11,21 +10,27 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        try (BufferedReader reader = new BufferedReader(new FileReader(bufferedReader.readLine()))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(bufferedReader.readLine()));
+             FileWriter writer = new FileWriter(reader.readLine())) {
             ArrayList<String> list = new ArrayList<>();
             while (reader.ready()) {
                 String string = reader.readLine();
                 list.add(string);
             }
-                if (args[0].equals("-u")) {
-                    String idIsArgs = args[1];
-                  for (String str : list){
-                      String idIsFile = str.substring(0, 8).trim();
-                      if (idIsArgs.equals(idIsFile)){
+            if (args[0].equals("-u")) {
+                String idIsArgs = args[1];
+                String line = args[2];
+                for (String str : list) {
+                    String idIsFile = str.substring(0, 8).trim();
+                    if (idIsArgs.equals(idIsFile)) {
 
-                      }
-                  }
+
+                    }
+                }
             }
         }
     }
 }
+
+
+
