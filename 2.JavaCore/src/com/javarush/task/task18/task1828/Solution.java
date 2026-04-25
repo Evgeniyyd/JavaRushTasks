9dcc9bd28bac9b705b30ef243766aa4863077c18
Evgeniyyd <brai.kel@icloud.com>
2026-04-25 21:31:54 +0300

    протестить и в случае ошибки исправить

--- 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
@@ -19,26 +19,36 @@ public class Solution {
                 list.add(string);
             }
             if (args.length > 0 && args[0].equals("-u")) {
-                String format = " ";
+                String format = "";
                 String idIsArgs = args[1];
                 for (int i = 0; i < list.size(); i++) {
-                    String idIsFile = idIsArgs.substring(0, 8).trim();
+                    String idIsFile = list.get(i).substring(0, 8).trim();
                     if (idIsArgs.equals(idIsFile)) {
                         String productName = args[2];
                         String price = args[3];
                         String quantity = args[4];
                         format = String.format("%-8.8s%-30.30s%-8.8s%-4.4s",
                                 idIsArgs, productName, price, quantity);
-                        list.set(i,format);
+                        list.set(i, format);
 
                     }
                     writer.write(format);
                 }
             }
+            if (args.length > 0 && args[0].equals("-d")) {
+                String idIsArgs = args[1];
+                for (int i = 0; i < list.size(); i++) {
+                    String idIsFile = list.get(i).substring(0, 8).trim();
+                    if (idIsArgs.equals(idIsFile)) {
+                        list.remove(i);
+                        i--;
+                    }
+                }
+            }
         }
-
     }
 }
+
 
 
 
