4d18012a5b97ee2d7eb7e308a789e11238c12dcc
Evgeniyyd <brai.kel@icloud.com>
2026-04-26 12:25:24 +0300

    переписать на соллекцию Map

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1828/Main.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task18.task1828;
+
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String[] array = {"-d","198479"};
+        Solution.main(array);
+    }
+}
--- 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1828/Solution.java
@@ -9,17 +9,17 @@ import java.util.ArrayList;
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        try (BufferedReader reader = new BufferedReader(new FileReader(bufferedReader.readLine()));
-             FileWriter writer = new FileWriter(reader.readLine())) {
-            ArrayList<String> list = new ArrayList<>();
+    public static void main1(String[] args) throws IOException {
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = console.readLine();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+           ArrayList<String> list = new ArrayList<>();
+
             while (reader.ready()) {
                 String string = reader.readLine();
                 list.add(string);
             }
             if (args.length > 0 && args[0].equals("-u")) {
-                String format = "";
                 String idIsArgs = args[1];
                 for (int i = 0; i < list.size(); i++) {
                     String idIsFile = list.get(i).substring(0, 8).trim();
@@ -27,15 +27,12 @@ public class Solution {
                         String productName = args[2];
                         String price = args[3];
                         String quantity = args[4];
-                        format = String.format("%-8.8s%-30.30s%-8.8s%-4.4s",
+                        String format = String.format("%-8.8s%-30.30s%-8.8s%-4.4s",
                                 idIsArgs, productName, price, quantity);
-                        list.set(i, format);
-
+                       list.set(i, format);
                     }
-                    writer.write(format);
                 }
-            }
-            if (args.length > 0 && args[0].equals("-d")) {
+            } else if (args.length > 0 && args[0].equals("-d")) {
                 String idIsArgs = args[1];
                 for (int i = 0; i < list.size(); i++) {
                     String idIsFile = list.get(i).substring(0, 8).trim();
@@ -45,10 +42,15 @@ public class Solution {
                     }
                 }
             }
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+                for (String line : list) {
+                    writer.write(line);
+                    writer.newLine();
+                }
+            }
         }
     }
 }
-
 
 
 
