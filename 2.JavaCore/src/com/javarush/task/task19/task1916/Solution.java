3338878208759a24ec4285960419755e088c3367
Evgeniyyd <brai.kel@icloud.com>
2026-05-08 00:16:43 +0300

    сделать проверку почему не добавляется последнияя строка

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1916/Solution.java
@@ -0,0 +1,71 @@
+package com.javarush.task.task19.task1916;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Отслеживаем изменения
+*/
+
+public class Solution {
+    public static List<LineItem> lines = new ArrayList<LineItem>();
+
+    public static void main(String[] args) throws IOException {
+        List<String> oneList = new ArrayList<>();
+        List<String> twoList = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try (BufferedReader fileOne = new BufferedReader(new FileReader(reader.readLine()));
+             BufferedReader fileTwo = new BufferedReader(new FileReader(reader.readLine()))) {
+
+            while (fileOne.ready()) {
+                String readOne = fileOne.readLine();
+                oneList.add(readOne);
+            }
+            while (fileTwo.ready()) {
+                String readTwo = fileTwo.readLine();
+                twoList.add(readTwo);
+            }
+        }
+        while (!oneList.isEmpty() && !twoList.isEmpty()) {
+            if (oneList.get(0).equals(twoList.get(0))) {
+                lines.add(new LineItem(Type.SAME, oneList.get(0)));
+                oneList.remove(0);
+                twoList.remove(0);
+            } else if (oneList.get(0).equals(twoList.get(1))) {
+                lines.add(new LineItem(Type.ADDED, twoList.get(0)));
+                twoList.remove(0);
+            } else if (oneList.get(1).equals(twoList.get(0))) {
+                lines.add(new LineItem(Type.REMOVED, oneList.get(0)));
+                oneList.remove(0);
+            }
+
+        }
+        lines.forEach(System.out::println);
+    }
+
+
+    public static enum Type {
+        ADDED,        //добавлена новая строка
+        REMOVED,      //удалена строка
+        SAME          //без изменений
+    }
+
+    public static class LineItem {
+        public Type type;
+        public String line;
+
+        public LineItem(Type type, String line) {
+            this.type = type;
+            this.line = line;
+        }
+
+        @Override
+        public String toString() {
+            return type + " " + line;
+        }
+    }
+}
