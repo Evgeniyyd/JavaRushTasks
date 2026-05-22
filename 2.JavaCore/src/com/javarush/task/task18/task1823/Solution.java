137307242a607faa112b59f9f9add231c8293364
Evgeniyyd <brai.kel@icloud.com>
2026-05-22 21:07:11 +0300

    проверить решение

--- 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1823/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.task.task18.task1823;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,47 +9,51 @@ import java.util.Map;
 */
 
 public class Solution {
-    public static Map<String, Integer> resultMap = new HashMap<String, Integer>();
+    public final static Map<String, Integer> resultMap = new HashMap<String, Integer>();
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
         while (true) {
-            String line = reader.readLine();
             if (line.equals("exit")) {
                 break;
             }
-            ReadThread readThread = new ReadThread(line);
-            readThread.start();
+            ReadThread thread = new ReadThread(line);
+            thread.start();
         }
     }
 
     public static class ReadThread extends Thread {
-        public String name;
+        String name;
 
         public ReadThread(String fileName) {
             this.name = fileName;
+            //implement constructor body
         }
 
         @Override
         public void run() {
             try {
                 FileInputStream inputStream = new FileInputStream(name);
-                int bytes;
-                int[] array = new int[256];
-                while ((bytes = inputStream.read()) != -1) {
-                    array[bytes]++;
+                int[] arrys = new int[256];
+                int count;
+                while ((count = inputStream.read()) != -1) {
+                    arrys[count]++;
                 }
-                int maxCount = 0;
+                int countByte = 0;
                 int maxByte = 0;
-                for (int i = 0; i < array.length; i++) {
-                    if (array[i] > maxCount) {
-                        maxCount = array[i];
+                for (int i = 0; i < arrys.length; i++) {
+                    if (arrys[i] > countByte) {
+                        countByte = arrys[i];
                         maxByte = i;
                     }
                 }
-                resultMap.put(name, maxByte);
+                synchronized (resultMap) {
+                    resultMap.put(name, maxByte);
+                }
                 inputStream.close();
-            } catch (IOException e) {
+            } catch (Exception e) {
                 throw new RuntimeException(e);
             }
         }
