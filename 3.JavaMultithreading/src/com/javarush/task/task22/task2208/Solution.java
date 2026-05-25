0eb9d6635d93db577f1e599f745b647b1080da1e
Evgeniyyd <brai.kel@icloud.com>
2026-05-25 16:12:56 +0300

    исправить ньюанс для валидации

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2208/Solution.java
@@ -0,0 +1,46 @@
+package com.javarush.task.task22.task2208;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/* 
+Формируем WHERE
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name", "Ivanov");
+        map.put("country", "Ukraine");
+        map.put("city", "kiev");
+        map.put("age", null);
+        String query = getQuery(map);
+        System.out.println(query);
+
+    }
+
+    public static String getQuery(Map<String, String> params) {
+        if (params == null || params.isEmpty()) {
+            return "";
+        }
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            String value = null;
+            String key = entry.getKey();
+            if (value != null) {
+                builder.append(key).append(" =")
+                        .append(" ").append("'")
+                        .append(value).append("'")
+                        .append(" ").append("and").append(" ");
+            }
+        }
+        int indexOf = builder.lastIndexOf(" and");
+
+        builder.delete(indexOf, builder.length());
+
+        return builder.toString();
+    }
+}
+
