7707e6a4bca3dfac0496f51253b6352ad163ce05
Evgeniyyd <brai.kel@icloud.com>
2026-06-06 21:09:03 +0300

    изменить получения и формирования даты

--- 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
@@ -1,9 +1,16 @@
 package com.javarush.task.task19.task1904;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.*;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 /* 
 И еще один адаптер
@@ -11,14 +18,15 @@ import java.util.*;
 
 public class Solution {
 
-    public static void main(String[] args) throws IOException {
-        PersonScannerAdapter personAdapter = new PersonScannerAdapter(new Scanner("file"));
-        personAdapter.read();
-        System.out.println(personAdapter);
+    public static void main(String[] args) {
+        var date = new Date("21.29.2008");
+        Person person = new Person("bdfyjd", "dkssdjsdksdk", "ksdkdsk", date);
+        System.out.println(person);
     }
 
     public static class PersonScannerAdapter implements PersonScanner {
-        private Scanner fileScanner;
+        private final Scanner fileScanner;
+
 
         public PersonScannerAdapter(Scanner fileScanner) {
             this.fileScanner = fileScanner;
@@ -26,20 +34,30 @@ public class Solution {
 
         @Override
         public Person read() throws IOException {
-            try (FileInputStream inputStream = new FileInputStream(fileScanner.nextLine())) {
-                StringBuilder builder = new StringBuilder();
-                int count;
-                while ((count = inputStream.read()) != -1) {
-
-                }
-                return null;
+            String birthDate;
+            String name;
+            Person person;
+            try (FileInputStream fileInputStream = new FileInputStream(fileScanner.nextLine());
+                 InputStreamReader reader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
+                 BufferedReader fileReader = new BufferedReader(reader)) {
+                String string = fileReader.readLine();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+                name = string.replaceAll("[0-9]", "").trim();
+                String[] split = name.split(" ");
+                String surname = split[0];
+                String names = split[1];
+                String patronymic = split[2];
+                birthDate = string.replaceAll("\\D ", "").trim();
+                LocalDate parse = LocalDate.parse(birthDate,formatter);
+                Date from = Date.from(Instant.from(parse));
+                person = new Person(surname,names,patronymic,from);
             }
+            return person;
         }
 
         @Override
         public void close() throws IOException {
-            this.fileScanner = fileScanner;
-
+            fileScanner.close();
 
         }
     }
