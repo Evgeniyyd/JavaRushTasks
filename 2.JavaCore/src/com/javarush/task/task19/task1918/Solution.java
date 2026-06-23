27c96a755a10cbb374908d6dc311ad55a8f82fdd
Evgeniyyd <brai.kel@icloud.com>
2026-06-23 13:06:23 +0300

    готово

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -1,5 +1,10 @@
 package com.javarush.task.task19.task1918;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+import org.jsoup.select.Elements;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -19,31 +24,38 @@ public class Solution {
             while ((line = reader.readLine()) != null) {
                 builder.append(line);
             }
-            int count = 0;
-            int nexOpen = 0;
-            int nexClosed = 0;
-            int startIndex = 0;
-            int tagStart = 0;
-            int tagEnd = 0;
-            String open = "<" + args[0];
-            String close = "</" + args[0] + ">";
-            String str = builder.toString();
-            while (startIndex != -1) {
-                nexOpen = str.indexOf(open, startIndex);
-                nexClosed = str.indexOf(close, startIndex + 1);
-                if (nexOpen < nexClosed && nexOpen != -1) {
-                    startIndex = nexOpen + 1;
-                    tagStart = nexOpen;
-                    count++;
-                }else  if (nexOpen > nexClosed) {
-                    startIndex = nexClosed;
-                    count--;
-                }  if (count == 0) {
-                    tagEnd = nexClosed;
-                    String substring = str.substring(tagStart, tagEnd + close.length());
-                    System.out.println(substring);
-                }
-            }
+//            int count = 0;
+//            int nexOpen = 0;
+//            int nexClosed = 0;
+//            int startIndex = 0;
+//            int tagStart = 0;
+//            int tagEnd = 0;
+//            String open = "<" + args[0];
+//            String close = "</" + args[0] + ">";
+//            String str = builder.toString();
+//            while (startIndex != -1) {
+//                nexOpen = str.indexOf(open, startIndex);
+//                nexClosed = str.indexOf(close, startIndex + 1);
+//                if (nexOpen < nexClosed && nexOpen != -1) {
+//                    startIndex = nexOpen + 1;
+//                    tagStart = nexOpen;
+//                    count++;
+//                } else if (nexOpen > nexClosed) {
+//                    startIndex = nexClosed;
+//                    count--;
+//                }
+//                if (count == 0) {
+//                    tagEnd = nexClosed;
+//                    String substring = str.substring(tagStart, tagEnd + close.length());
+//                    System.out.println(substring);
+//                }
+//            }
+
+            Document document = Jsoup.parse(builder.toString(), "", Parser.xmlParser());
+            Elements elements = document.select(args[0]);
+            elements.forEach(System.out::println);
+
+
         }
     }
 }
