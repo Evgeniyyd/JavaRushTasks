5dd74c1d7fcafbcda15e67df95fb644aba54eba2
Evgeniyyd <brai.kel@icloud.com>
2026-06-27 20:58:51 +0300

    дописать логику

--- 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
@@ -3,7 +3,8 @@ package com.javarush.task.task19.task1919;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -13,26 +14,27 @@ import java.util.TreeMap;
 
 public class Solution {
     public static void main(String[] args) throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-             BufferedReader file = new BufferedReader(new FileReader(reader.readLine()))) {
+        try(BufferedReader reader = new BufferedReader(new FileReader(args[0]))){
             String line;
+            Map<String,Double> map = new TreeMap<>();
             String name;
             double parsed;
-            Map<String, Double> map = new TreeMap<>();
-            while ((line = file.readLine()) != null) {
+            while ((line = reader.readLine())!= null){
                 String[] split = line.split(" ");
-                name = split[0];
-                parsed = Double.parseDouble(split[1]);
-                if (map.containsKey(name)) {
-                    map.put(name, map.get(name) + parsed);
-                } else {
-                    map.put(name, parsed);
+                 name = split[0];
+                 parsed = Double.parseDouble(split[1]);
+                if (map.containsKey(name)){
+                    map.put(name,parsed);
+                    }else {
+                    map.put(name,map.get(name)+parsed);
                 }
             }
-            for (Map.Entry<String, Double> entry : map.entrySet()) {
-                System.out.println(entry.getKey() + " " + entry.getValue());
-            }
+            Collection<Double> values = map.values();
+            map.entrySet()
+                    .stream()
+                    .map(Map.Entry::getValue);
+
+
         }
     }
 }
-
