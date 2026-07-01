b88c488ac5a9da8e1582944a5201cf7dd414b3d7
Evgeniyyd <brai.kel@icloud.com>
2026-07-01 18:15:22 +0300

    решено

--- 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 */
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main1(String[] args) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
             Map<String, Double> collect = reader.lines().filter(Objects::nonNull)
                     .map(lines -> lines.split(" "))
@@ -24,6 +24,28 @@ public class Solution {
                     .sorted(Map.Entry.comparingByValue())
                     .map(entry -> entry.getKey() + " " + entry.getValue())
                     .forEach(System.out::println);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+            Map<String, Double> map = new TreeMap<>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] split = line.split(" ");
+                String name = split[0];
+                double parsed = Double.parseDouble(split[1]);
+                if (!map.containsKey(name)) {
+                    map.put(name, parsed);
+                } else {
+                    map.put(name, map.get(name) + parsed);
+                }
+            }
+            List<Map.Entry<String, Double>> list = new ArrayList<>(map.entrySet());
+                list.sort(Map.Entry.comparingByKey());
+            for (Map.Entry<String, Double> entry : list) {
+                System.out.println(entry.getKey()+" "+ entry.getValue());
+            }
         }
     }
 }
