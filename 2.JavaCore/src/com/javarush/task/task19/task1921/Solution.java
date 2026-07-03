b75edb4a898879ca404935b60c82d9a06f266257
Evgeniyyd <brai.kel@icloud.com>
2026-07-03 21:14:00 +0300

    решить задачу

--- 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
@@ -1,8 +1,9 @@
 package com.javarush.task.task19.task1921;
 
-import java.io.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 /* 
@@ -12,21 +13,22 @@ import java.util.*;
 public class Solution {
     public static final List<Person> PEOPLE = new ArrayList<Person>();
 
-    public static void main(String[] args) throws IOException, ParseException {
-        SimpleDateFormat format = new SimpleDateFormat("dd MM yyyy");
-        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
-            while (reader.ready()) {
-                String line = reader.readLine();
-                String[] split = line.split(" ");
-                if (split.length >= 4) {
-                    String day = split[split.length - 3];
-                    int indexOf = line.indexOf(day);
-                    String name = line.substring(0, indexOf).trim();
-                    String birthDate = line.substring(indexOf).trim();
-                    Date parse = format.parse(birthDate);
-                    PEOPLE.add(new Person(name, parse));
-                }
+    public static void main(String[] args) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(args[0]))) {
+            String string = fileReader.readLine();
+            String[] split = string.split(" ");
+            String year = split[split.length - 1];
+            String month = split[split.length - 2];
+            String day = split[split.length - 3];
+
+
+
+            for (int i = 0; i < split.length-3; i++) {
+                builder.append(split[i]);
             }
+
         }
     }
 }
